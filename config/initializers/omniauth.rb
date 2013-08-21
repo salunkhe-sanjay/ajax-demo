7385b1cebdd2e66e3f98867e804cde0f4adc7877
sanjay-salunkhe <sanjaysalunkhe14@gmail.com>
2013-08-21 15:10:53 +0530

    my first commit for ajax demo

---
+++ config/initializers/omniauth.rb
@@ -0,0 +1,3 @@
+Rails.application.config.middleware.use OmniAuth::Builder do
+  provider :yahoo, 'dj0yJmk9UTVCYVJyTXV3bk5HJmQ9WVdrOVRHTnRkMmR6Tm1jbWNHbzlNakV4TmpVM016azJNZy0tJnM9Y29uc3VtZXJzZWNyZXQmeD1lMw--', 'd789318b08975e2e4a8e2ec44d53568c08286178'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Demo::Application.config.secret_token = '84410df07f8cee4a58660b0603f27d4a0f7acf8f6da655f97e34d0139f82b43a2db0c5406bef1b2e3a4040ad91a688fb3a89c886fd63f0f97ecdc321126ca098'
